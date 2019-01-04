0fecd0cfdb9731dd2cb82af8c3cf774bdb984912
HaoChuan9421 <hc199421@gmail.com>
2019-01-04 16:32:54 +0800

    重构服务更新逻辑，使得过程更加无缝

--- server/index.js
+++ server/index.js
@@ -1,82 +1,131 @@
 const express = require('express');
 const consola = require('consola');
-const { Nuxt, Builder } = require('nuxt');
-const { spawn } = require('child_process');
 const bodyParser = require('body-parser'); // 用于添加请求体到 request 参数中, req.body
-const crypto = require('crypto');
+const crypto = require('crypto'); // 用于计算签名
+const { exec } = require('child_process'); // 用于执行子进程
+const { Nuxt, Builder } = require('nuxt');
+
+const app = express();
 const port = process.env.PORT || 3000;
+var server = null; // 应用的 http server 实例
+var render = null; // nuxt render 中间件
+var upgrading = false; // 一个标记应用是否正在升级的 flag
 
-var appServer, // 应用主服务
-  tmpServer, // 当应用更新时启动的临时服务，用于提示访问者系统正在升级中
-  upgrading; // 应用是否正在升级
+app.set('port', port);
 
-build();
+// 解析 application/json
+app.use(bodyParser.json());
+
+// 系统升级提示页的背景图片
+app.get('/upgrading_bg.jpg', function(req, res) {
+  res.sendFile('./upgrading_bg.jpg', { root: __dirname });
+});
+
+// 拦截所以 get 请求，如果系统正在升级中，则返回提示页面
+app.get('*', function(req, res, next) {
+  if (upgrading) {
+    res.sendFile('./upgrading.html', { root: __dirname });
+  } else {
+    next();
+  }
+});
+
+// 订阅来自 git 服务器 的 Webhooks 请求（post 类型）
+app.post('/webhooks', function(req, res) {
+  // 使用 secret token 对该 API 的调用进行鉴权, 详细文档: https://developer.github.com/webhooks/securing/
+  const SECRET_TOKEN = 'b65c19b95906e027c5d8';
+  // 计算签名
+  const signature = `sha1=${crypto
+    .createHmac('sha1', SECRET_TOKEN)
+    .update(JSON.stringify(req.body))
+    .digest('hex')}`;
+  // 验证签名和 Webhooks 请求中的签名是否一致
+  const isValid = signature === req.headers['x-hub-signature'];
+  // 如果验证通过，返回成功状态并重启服务
+  if (isValid) {
+    res.status(200).end('Authorized');
+    upgrade();
+  } else {
+    // 鉴权失败，返回无权限提示
+    res.status(403).send('Permission Denied');
+  }
+});
+
+/**
+  预留一个接口，必要时用于手动执行一些命令，如通过发起下面的一段 Ajax 请求，进行 npm 包的升级并重新构建项目
+  var xhr = new XMLHttpRequest();
+  xhr.open('post', '/command');
+  xhr.setRequestHeader('access_token', 'b65c19b95906e027c5d8');
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  xhr.send(
+    JSON.stringify({
+      command: 'npm update',
+      reBuild: true
+    })
+  );
+ */
+app.post('/command', function(req, res) {
+  // 如果必要可以进行更严格的鉴权，这里只是一个示范
+  if (req.headers['access_token'] === 'b65c19b95906e027c5d8') {
+    // 如果是纯粹的重新构建，没有需要执行的命令，直接结束请求，不需要等待命令的执行结果
+    if (!req.body.command && req.body.reBuild) {
+      res.status(200).end('Authorized and rebuilding!');
+    }
+    // 执行命令，并返回命令的执行结果
+    execCommand(req.body.command, req.body.reBuild, function(
+      error,
+      stdout,
+      stderr
+    ) {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).json({ stdout, stderr });
+      }
+    });
+  } else {
+    res.status(403).send('Permission Denied');
+  }
+});
 
 /**
  * 根据配置，构建项目
  */
 async function build() {
-  const app = express();
-
-  // 解析 application/json
-  app.use(bodyParser.json());
-
-  app.set('port', port);
-
+  if (upgrading) {
+    return;
+  }
+  upgrading = true;
   // 导入并设置 Nuxt.js 参数
   let config = require('../nuxt.config.js');
   config.dev = !(process.env.NODE_ENV === 'production');
-
-  // 在生成环境中订阅来自 GitHub 的 Webhooks 请求（post 类型）
-  config.dev ||
-    app.post('/webhooks', function(req, res) {
-      // 使用 secret token 对该 API 的调用进行鉴权, 详细文档: https://developer.github.com/webhooks/securing/
-      const SECRET_TOKEN = 'b65c19b95906e027c5d8';
-      // 计算签名
-      const hash = `sha1=${crypto
-        .createHmac('sha1', SECRET_TOKEN)
-        .update(JSON.stringify(req.body))
-        .digest('hex')}`;
-      // 验证签名和 Webhooks 请求的请求头中的是否一致
-      const isValid = hash === req.headers['x-hub-signature'];
-      // 如果验证通过，返回成功状态并重启服务
-      if (isValid) {
-        res.status(200).end();
-        restart();
-      } else {
-        // 鉴权失败，返回无权限提示
-        res.status(403).send('Permission Denied');
-      }
-    });
   // 初始化 Nuxt.js
   const nuxt = new Nuxt(config);
-
   // 构建应用，得益于环境变量 NODE_ENV，在开发环境和生产环境下这个构建的表现会不同
   const builder = new Builder(nuxt);
   await builder.build();
-
-  // 添加 nuxt 中间件到 express
-  app.use(nuxt.render);
-
-  if (appServer) {
-    tmpServer &&
-      tmpServer.listening &&
-      tmpServer.close(error => {
-        if (error) {
-          return;
-        }
-        createAppServer(app, port);
-      });
-  } else {
-    createAppServer(app, port);
-  }
+  // 更新 render 中间件
+  render = nuxt.render;
+  // 构建完毕，将 flag 置反
+  upgrading = false;
+  // 如果是初次构建，则创建 http server
+  server || createServer();
 }
 
 /**
- * 创建应用服务，并将 upgrading 变量设置为 false
+ * 创建应用的 http server
  */
-function createAppServer(app, port) {
-  appServer = app.listen(port, function(error) {
+function createServer() {
+  /**
+   * 添加 nuxt 中间件到 express：
+   * 重新构建之后，中间件会发生变化
+   * 这种处理方式的好处就在于 express 使用的总是最新的 nuxt.render
+   */
+  app.use(function() {
+    render.apply(this, arguments);
+  });
+  // 启动服务
+  server = app.listen(port, function(error) {
     if (error) {
       return;
     }
@@ -84,50 +133,27 @@ function createAppServer(app, port) {
       message: `Server listening on http://localhost:${port}`,
       badge: true
     });
-    upgrading = false;
   });
 }
 
 /**
- * 创建临时服务，用于提示访问者系统正在升级中，并重新构建应用
+ * 从 git 服务器拉取最新代码，更新 npm 依赖，并重新构建项目
  */
-function createTmpServer() {
-  const app = express();
-  app.get('/bg.jpg', function(req, res) {
-    res.sendFile('./bg.jpg', { root: __dirname });
-  });
-  app.get('*', function(req, res) {
-    res.sendFile('./upgrading.html', { root: __dirname });
-  });
-
-  tmpServer = app.listen(port, function(error) {
-    if (error) {
-      return;
-    }
-    console.log(`tmpServer listening on http://localhost:${port}`);
-    build();
-  });
+function upgrade() {
+  execCommand('git pull -f && npm install', true);
 }
 
 /**
- * 关闭应用服务，并启动一个临时的提示服务
+ * 创建子进程，执行命令
+ * @param {String} command 需要执行的命令
+ * @param {Boolean} reBuild 执行完毕后，是否重新构建应用
+ * @param {Function} callback 执行命令后的回调
  */
-function restart() {
-  if (upgrading) {
-    return;
-  }
-  upgrading = true;
-  // 创建一个子进程，从 GitHub 拉取最新的代码
-  const subprocess = spawn('git', ['pull', '-f'], { stdio: [0, 1, 2] });
-  subprocess.on('close', () => {
-    // 拉取完成后，关闭应用服务，并创建一个临时服务
-    appServer &&
-      appServer.listening &&
-      appServer.close(error => {
-        if (error) {
-          return;
-        }
-        createTmpServer();
-      });
-  });
+async function execCommand(command, reBuild, callback) {
+  command && (await exec(command, callback));
+  // 根据配置文件，重新构建项目
+  reBuild && build();
 }
+
+// 初次构建
+build();
